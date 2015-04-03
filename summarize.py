8ea2a67018cea385c753494001cd4d330c4ed984
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-04-02 23:16:26 -0400

    added frequency counts to likelyFeatures for each subject

--- summarize.py
+++ summarize.py
@@ -9,25 +9,38 @@ class Analysis():
 
 
     def loadFile(self, path = 'bills/hconres/hconres1/data.json' ):
-        skipped = 0
         with open(path) as data_file:
             data = json.load(data_file)
 
             try:
                 self.feature_dict[data["bill_id"]] = data["subjects"] + \
                                                     [data["status"] == "ENACTED:SIGNED"]
-            except:
-                sys.stdout.flush()
-                sys.stdout.write("\rskipped %d files" % skipped)
-                skipped += 1
+            except: pass
 
     def populateFeatureDict(self, path = "bills"):
+        print("loading files... ")
         for path, dirs, files in os.walk("bills"):
             for data_file in files:
                 if data_file[-4:] == "json":
                     self.loadFile(path + "/" + data_file)
+        print("\tDONE")
 
         # pprint(self.feature_dict)
 
+    # attempts to answer the question "what types of bills get passed"
+    # by looking at the subjects and seeing what proportion of each get passed
+    def likelyFeatures(self):
 
-Analysis()
+        # compile the features into map with {subject: (count, passed)}
+
+        subjects = {}
+        for bill in self.feature_dict.keys():
+            for subject in self.feature_dict[bill][:-1]:
+                if subject not in subjects:
+                    subjects[subject] = [0, 0]
+                subjects[subject][0] += 1
+                subjects[subject][1] += 1 if self.feature_dict[bill][-1] else 0
+
+        pprint(subjects)
+
+Analysis().likelyFeatures()
