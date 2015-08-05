2a971dcf3f15161c728b0b612afffa6e1e463d35
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-08-04 21:16:12 -0700

    added generation of summary report

--- demo.py
+++ demo.py
@@ -1,17 +1,34 @@
 from pprint import pprint
 from summarize import Analysis
+import csv
+from pprint import pprint
 
 analysis = Analysis()
 
 def top10Subjects():
     top10 = analysis.likelyFeatures()
+    out = [['proportion', 'subject', 'enacted', 'proposed']]
 
-    message = "## Top 10 Most Passed Subjects ##"
-    print("#" * len(message) + "\n" + message + "\n" + "#" * len(message))
-    header = ("%10s" % "proportion") + ("%51s" % "subject") + ("%8s" % "enacted") + ("%9s" % "proposed") 
-    print(header)
+    # message = "## Top 10 Most Passed Subjects ##"
+    # out += "#" * len(message) + "\n" + message + "\n" + "#" * len(message) + "\n"
+    # header = ("%10s" % "proportion") + ("%51s" % "subject") + ("%8s" % "enacted") + ("%9s" % "proposed")  + "\n"
+    # out += header
 
+    # for piece in top10:
+    #     out += ("%.8f" % piece[0] + "%50s" % piece[1] + "%7d" % piece[2] + "%8d" % piece[3])
+
+    # print(out)
+    # with open("report.txt", "w") as f:
+    #     f.close()
     for piece in top10:
-        print("%.8f" % piece[0], "%50s" % piece[1], "%7d" % piece[2], "%8d" % piece[3])
+            out += [list(piece)]
+
+    with open('report.csv', 'w+') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in out:
+            spamwriter.writerow(row)
+        
+
+    pprint(out)
 
 top10Subjects()
--- summarize.py
+++ summarize.py
@@ -18,17 +18,23 @@ class Analysis():
 
     def populateFeatureDict(self, path = "data"):
         print("loading files... ")
+        count = 0
         for path, dirs, files in os.walk(path):
-            file_count = len(files)
             for data_file in files:
                 if data_file[-4:] == "json":
                     self.loadFile(path + "/" + data_file)
+
+            count += 1
+            sys.stdout.flush()
+            sys.stdout.write("\rSeen %d documents" % count)
+            
         print(coloredMessage("\tDone", "green"))
 
     # attempts to answer the question "what types of bills get passed"
     # by looking at the subjects and seeing what proportion of each get passed
     def likelyFeatures(self):
 
+        seen = 0
         # compile the features into map with {subject: (count, passed)}
         subjects = {}
         for bill in self.feature_dict.keys():
@@ -39,7 +45,7 @@ class Analysis():
                 subjects[subject][1] += 1 if self.feature_dict[bill][-1] else 0
 
         # pprint(subjects)
-        return(sortedHash(subjects)[::-1])
+        return(sortedHash(subjects)[::-1][:10])
 
 if __name__ == '__main__':
     pprint(Analysis().likelyFeatures())
