4ecc77bc11a28febf0df0876e331b9f5d252c592
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-11 12:30:09 -0500

    added version of generation of denormalized dataset in an attempt to remove repeated sql queries

--- helpers/distill.py
+++ helpers/distill.py
@@ -1,7 +1,7 @@
+from __future__ import print_function
 import os
 import json
 import csv
-
 
 '''
     The Basic ERD for bills (at least for our purposes) is as follows
@@ -41,3 +41,34 @@ def distill_3NF():
                 # this is the case of malformed JSON
                 pass
 
+def distill_1NF():
+    bill_csv =  open('data_distilled/distilled_bills_1NF.csv', 'w')
+
+    for path, dirs, files in os.walk("data"):
+        for data_file in files:
+            bill_path = path + "/" + data_file
+
+            data_file = open(bill_path)
+            data = json.load(data_file)
+
+            try:
+
+                row_buffer = data["sponsor"]["name"] + "\t" + process_states_simple(data["status"]) + "\t"
+                
+                # bill_csv_row = [data["bill_id"], data["sponsor"]["name"], process_states_simple(data["status"])]
+                # print(bill_csv_row)
+
+                for subject in data["subjects"]:
+                    row_buffer += subject + "|"
+
+                row_buffer = row_buffer[:-1]
+
+                # print(row_buffer)
+                bill_csv.write(row_buffer + "\n")
+            except:
+                # this is the case of malformed JSON
+                pass
+
+    bill_csv.close()
+
+distill_1NF()
